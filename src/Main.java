ddac004c2863903321531cf76a2dd12b48549c15
Nicolas Schapeler <nicoschap@live.fr>
2020-04-05 14:34:24 +0200

    added remaining code

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,121 @@
 import com.javaToken.JavaToken;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.abi.datatypes.generated.Uint256;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCall;
 import org.web3j.protocol.core.methods.response.EthGasPrice;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.FastRawTransactionManager;
+import org.web3j.tx.TransactionManager;
 import org.web3j.tx.gas.ContractGasProvider;
+import org.web3j.utils.Numeric;
 
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // Ganache port is 7545
         Web3j w3 = Web3j.build(new HttpService("HTTP://127.0.0.1:7545"));
-        // Private key taken from Ganache test network, NEVER share your private Key with anyone
-        String privateKey = "91530b694c784cc79da98a19862932488521790d779a75b29744549e037d6e4a";
+
+        String privateKey = "YourPrivateKeyHere";
         Credentials credentials = Credentials.create(privateKey);
 
-        EthGasPrice ethGasPrice = w3.ethGasPrice().send();
-        BigInteger gasPrice = ethGasPrice.getGasPrice();
+        String contractAddress = "ContractAddressHere";
 
-        // Deploy the contract
-        JavaToken javaToken = deploy(gasPrice, BigInteger.valueOf(2000000),
-                BigInteger.valueOf(100000),credentials,w3);
+        // Deploy the contract/Fetch an already deployed contract
+
+        JavaToken javaToken = getDeployedContract(contractAddress, credentials, w3,
+                BigInteger.valueOf(200000));
+
+        // Do more smart contract things :)
+        // ...
+    }
+
+    static JavaToken deploy(BigInteger gasLimit,
+                            BigInteger initialSupply, Credentials credentials, Web3j w3)
+            throws Exception {
+        return JavaToken.deploy(w3, credentials, getContractGasProvider(getGasPrice(w3), gasLimit),
+                initialSupply).send();
+    }
+
+    static JavaToken getDeployedContract(String address, Credentials credentials, Web3j w3,
+                                         BigInteger gasLimit) throws IOException {
+        return JavaToken.load(address, w3, credentials, getContractGasProvider(getGasPrice(w3), gasLimit));
+    }
+
+    static BigInteger getBalance(String address, JavaToken javaToken) throws Exception {
+        return javaToken.balanceOf(address).send();
+    }
+
+    static TransactionReceipt transfer(String to, BigInteger value, JavaToken javaToken) throws Exception {
+        return javaToken.transfer(to,value).send();
+    }
+
+    static BigInteger getBalanceWithoutWrapper(String to, String contractAddress, Web3j w3) throws IOException {
+
+        // Define the function we want to invoke from the smart contract
+        Function function = new Function("balanceOf", Arrays.asList(new Address(to)),
+                Collections.emptyList());
+
+        // Encode it for the contract to understand
+        String encodedFunction = FunctionEncoder.encode(function);
+
+        /*
+         Send the request and wait for the response using eth call since
+         it's a read only transaction with no cost associated
+         */
+        EthCall response = w3.ethCall(
+                Transaction.createEthCallTransaction(to, contractAddress, encodedFunction),
+                DefaultBlockParameterName.LATEST).send();
+
+        return Numeric.toBigInt(response.getValue());
 
     }
 
-    static JavaToken deploy(BigInteger gasPrice, BigInteger gasLimit,
-                            BigInteger initialSupply, Credentials credentials, Web3j w3)
-            throws Exception {
+    static TransactionReceipt transferWithoutWrapper(String address, BigInteger value, String contractAddress,
+                                                     Credentials credentials, BigInteger gasLimit,
+                                                     Web3j w3) throws IOException {
 
-        return JavaToken.deploy(w3, credentials, new ContractGasProvider() {
+        // Define the function we want to invoke from the smart contract
+        Function function = new Function("transfer", Arrays.asList(new Address(address), new Uint256(value)),
+                Collections.emptyList());
+
+        // Encode it for the contract to understand
+        String encodedFunction = FunctionEncoder.encode(function);
+
+        /*
+         Need to use a TransactionManager here since transfer actually alters the state of the blockchain
+         and credentials are therefore relevant
+         */
+        TransactionManager transactionManager = new FastRawTransactionManager(w3, credentials);
+
+
+        // Send the transaction off using transactionManager and wait for the hash
+        String transactionHash = transactionManager.sendTransaction(getGasPrice(w3), gasLimit,
+                contractAddress, encodedFunction, BigInteger.ZERO).getTransactionHash();
+
+        // Fetch the transaction receipt
+        Optional<TransactionReceipt> transactionReceipt =
+                w3.ethGetTransactionReceipt(transactionHash).send().getTransactionReceipt();
+
+        if(transactionReceipt.isEmpty())
+            return null;
+        return transactionReceipt.get();
+
+    }
+
+    // Helper functions
+    static ContractGasProvider getContractGasProvider(BigInteger gasPrice, BigInteger gasLimit) {
+        return new ContractGasProvider() {
             @Override
             public BigInteger getGasPrice(String s) {
                 return gasPrice;
@@ -49,7 +135,12 @@ public class Main {
             public BigInteger getGasLimit() {
                 return gasLimit;
             }
-        },initialSupply).send();
+        };
     }
 
+    static BigInteger getGasPrice(Web3j w3) throws IOException {
+        EthGasPrice ethGasPrice = w3.ethGasPrice().send();
+        return ethGasPrice.getGasPrice();
+
+    }
 }
